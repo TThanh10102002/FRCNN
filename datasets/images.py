c4ec23700df83c97e8d0ec3f34d0a35e2dabd059
TThanh10102002 <98950714+TThanh10102002@users.noreply.github.com>
2023-03-30 07:57:01 +0700

    Update images.py

--- datasets/images.py
+++ datasets/images.py
@@ -22,6 +22,27 @@ def preprocess_vgg16(image):
 
 
 def load_img(url, min_dim_pixels = None, horizontal_flip = False):
+    """
+      Loads and preprocesses an image for use with VGG-16, which consists of
+      converting RGB to BGR and subtracting ImageNet dataset means from each
+      component. The image can be resized so that the minimum dimension is a
+      defined size, as recommended by Faster R-CNN. 
+      Parameters
+      ----------
+      url : str
+        URL (local or remote file) to load.
+      min_dimension_pixels : int
+        If not None, specifies the size in pixels of the smaller side of the image.
+        The other side is scaled proportionally.
+      horizontal_flip : bool
+        Whether to flip the image horizontally.
+      Returns
+      -------
+      np.ndarray, PIL.Image, float, Tuple[int, int, int]
+        Image pixels as float32, shaped as (channels, height, width); an image
+        object suitable for drawing and visualization; scaling factor applied to
+        the image dimensions; and the original image shape.
+      """
     data = imageio.imread(url, pilmode = 'RGB')
     img = Image.fromarray(data, mode='RGB')
     org_width, org_height = img.width, img.height
@@ -37,4 +58,4 @@ def load_img(url, min_dim_pixels = None, horizontal_flip = False):
 
     img_data = np.array(img).astype(np.float32)
     img_data = preprocess_vgg16(image=img_data)
-    return img_data, img, scale_factor, (img.shape[0], org_height, org_width)
+    return img_data, img, scale_factor, (img_data.shape[0], org_height, org_width)
