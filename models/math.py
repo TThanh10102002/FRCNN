a975aea650696c95e5f78c7024eb961ea9d23402
TThanh10102002 <98950714+TThanh10102002@users.noreply.github.com>
2023-03-31 17:21:55 +0700

    Update math.py

--- models/math.py
+++ models/math.py
@@ -4,24 +4,62 @@ import tensorflow as tf
 
 #Compute IoU for multibox parallel
 def iou(box1, box2):
+    """
+      Computes intersection-over-union (IoU) for multiple boxes in parallel.
+      Parameters
+      ----------
+      boxes1 : np.ndarray
+        Box corners, shaped (N, 4), with each box as (y1, x1, y2, x2).
+      boxes2 : np.ndarray
+        Box corners, shaped (M, 4).
+      Returns
+      -------
+      np.ndarray
+        IoUs for each pair of boxes in boxes1 and boxes2, shaped (N, M).
+    """
     # (N,1,2) and (M,2) -> (N,M,2) indicating top-left corners of box pairs
     top_left_point = np.maximum(box1[: , None , 0:2], box2[: , 0:2])
     bottom_right_point = np.minimum(box1[: , None , 2:4], box2[: , 2:4])
+    
     # (N,M) indicating whether top_left_x < bottom_right_x and top_left_y < bottom_right_y (meaning boxes may intersect)
     ord_mask = np.all(top_left_point < bottom_right_point, axis = 2)
+    
     # (N,M) indicating intersection area (bottom_right_x - top_left_x) * (bottom_right_y - top_left_y)
     intersection = ord_mask * np.prod(bottom_right_point - top_left_point, axis = 2)
+    
     # (N,) indicating areas of boxes1
-    area_1 = np.prod(box1[: , 2:4] - box1[: , 0:2], axis = 1)
+    area_1 = np.prod(box1[:, 2:4] - box1[:, 0:2], axis = 1)
+    
     # (M,) indicating areas of boxes2
-    area_2 = np.prod(box2[: , 2:4] - box2[: , 0:2], axis = 1)
+    area_2 = np.prod(box2[:, 2:4] - box2[:, 0:2], axis = 1)
+    
      # (N,1) + (M,) - (N,M) = (N,M), union areas of both boxes
-    union_area = area_1[: , None] + area_2 - intersection
+    union_area = area_1[:, None] + area_2 - intersection
     eps = 1e-7
     return intersection / (union_area + eps)
 
 
 def tf_iou(box1, box2):
+    """
+    Equivalent of intersection_over_union() but operates on tf.Tensors and
+    produces a TensorFlow graph suitable for use in a model. This code borrowed
+    from Matterport's MaskRCNN implementation:
+    https://github.com/matterport/Mask_RCNN
+    Parameters
+    ----------
+    boxes1: tf.Tensor
+        Box corners, shaped (N,4), with each box as (y1, x1, y2, x2).
+    boxes2: tf.Tensor
+        Box corners, shaped (M,4).
+    Returns
+    -------
+    tf.Tensor
+        Tensor of shape (N, M) containing IoU score between each pair of boxes.
+    """
+    # 1. Tile boxes2 and repeat boxes1. This allows us to compare
+    # every boxes1 against every boxes2 without loops.
+    # TF doesn't have an equivalent to np.repeat() so simulate it
+    # using tf.tile() and tf.reshape.
     box_1 = tf.reshape(tf.tile(tf.expand_dims(box1, 1), [1, 1, tf.shape(box2)[0]]), [-1, 4])
     box_2 = tf.tile(box2, [tf.shape(box1)[0], 1])
     #Compute intersection
@@ -111,5 +149,5 @@ def tf_convert_deltas_to_boxes(box_deltas, anchors, box_delta_means, box_delta_s
     # y2, x2                        
     boxes_bottom_right = center + 0.5 * size     
     # [ (N,2), (N,2) ] -> (N,4)                 
-    boxes = tf.concat([boxes_top_left, boxes_bottom_right], 1) 
+    boxes = tf.concat([boxes_top_left, boxes_bottom_right], axis = 1) 
     return boxes
